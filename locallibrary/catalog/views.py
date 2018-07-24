57db6b3c503f99de9237782a94f519ac7b55be0d
RXC8755 <ram_chiliveri@homedepot.com>
2018-07-23 21:02:58 -0400

    New changes for creating home page

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
-
+from .models import Book, Author, BookInstance, Genre
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    """
+    View function for home page of site.
+    """
+    # Generate counts of some of the main objects
+    num_books=Book.objects.all().count()
+    num_instances=BookInstance.objects.all().count()
+    # Available books (status = 'a')
+    num_instances_available=BookInstance.objects.filter(status__exact='a').count()
+    num_authors=Author.objects.count()  # The 'all()' is implied by default.
+    num_genres=Genre.objects.count()
+	
+    # Render the HTML template index.html with the data in the context variable
+    return render(
+        request,
+        'index.html',
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors,'num_genres':num_genres},
+    )
